4917992eb951971d6768c35d87dc78a4c24ea175
hayashiki <masayuki@hayashida.dev>
2021-12-13 18:00:55 +0900

    Fix typo

--- next.config.js
+++ next.config.js
@@ -6,7 +6,25 @@ const nextConfig = {
     },
     experimental: {
         sprFlushToDisk: false,
-    }
+    },
+    trailingSlash: true,
 }
 
 module.exports = nextConfig
+
+//const isProd = process.env.NODE_ENV === 'production'
+//
+// const nextConfig = {
+//     // target: 'serverless',
+//
+//     env: {
+//         VERSION: require('./package.json').version,
+//         FIREBASE_PROJECT_ID: 'oimonote',
+//     },
+//
+//     experimental: {
+//         sprFlushToDisk: false,
+//     }
+// }
+//
+// module.exports = nextConfig
