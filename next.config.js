ad76b4c3c6449d36c24991f69a8c1bd2d39450b2
hayashiki <masayuki@hayashida.dev>
2021-12-14 11:19:10 +0900

    Add blog contents try

--- next.config.js
+++ next.config.js
@@ -17,20 +17,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-//const isProd = process.env.NODE_ENV === 'production'
-//
-// const nextConfig = {
-//     // target: 'serverless',
-//
-//     env: {
-//         VERSION: require('./package.json').version,
-//         FIREBASE_PROJECT_ID: 'oimonote',
-//     },
-//
-//     experimental: {
-//         sprFlushToDisk: false,
-//     }
-// }
-//
-// module.exports = nextConfig
